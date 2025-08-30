28e560f75a75f4695f02d6b11222329e3c43700c
Akif Öztürk <66181296+Akif01@users.noreply.github.com>
2025-08-30 21:45:18 +0700

    Added loading to startGame button

--- src/components/StartGameButton.tsx
+++ src/components/StartGameButton.tsx
@@ -1,20 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./StartGameButton.module.css"
 
 interface StartGameButtonProps {
     text: string;
-    onStartGame: () => void;
+    onStartGame: () => Promise<void> | void;
     disabled: boolean;
 }
 
 export default function CashoutButton({ text, onStartGame, disabled }: StartGameButtonProps) {
+    const [isLoading, setIsLoading] = useState(false);
+
+    const onClicked = async () => {
+        try {
+            setIsLoading(true);
+            await onStartGame();
+        } finally {
+            setIsLoading(false);
+        }
+    };
+
     return (
         <button
             className={styles.startGameButton}
-            onClick={onStartGame}
-            disabled={disabled}
+            onClick={onClicked}
+            disabled={disabled || isLoading}
         >
-            {text}
+            {isLoading ? "Loading..." : text}
         </button>
     );
 }
